7c5da39db7feb1261bd78458c36f2622494debfa
Thomas <tbeckman1234@gmail.com>
2023-09-26 12:03:04 -0600

    final build

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -2,42 +2,25 @@
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { TiDelete } from 'react-icons/ti'
+import { useTransition } from "react"
+import { deleteTicket } from "../actions"
 
 export default function DeleteButton({ id }) {
-    const [isLoading, setIsLoading] = useState(false)
-    const router = useRouter()
-
-    const handleClick = async () => {
-        setIsLoading(true)
-        
-        const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-            method: 'DELETE'
-        })
-        const json = await res.json()
-
-        if (json.error) {
-            console.log(error.message)
-            setIsLoading(false)
-        }
-        if (!json.error) {
-            router.refresh()
-            router.push('/tickets')
-        }
-    }
+    const [isPending, startTransition] = useTransition()
 
   return (
     <button 
         className="btn-primary"
-        onClick={handleClick}
-        disabled={isLoading}
+        onClick={() => startTransition(() => deleteTicket(id))}
+        disabled={isPending}
     >        
-        {isLoading && (
+        {isPending && (
             <>
                 <TiDelete />
                 Deleting...
             </>
         )}
-        {!isLoading && (
+        {!isPending && (
             <>
                 <TiDelete />
                 Delete Ticket            
--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,76 +1,36 @@
-"use client"
-
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import SubmitButton from '@/app/components/SubmitButton'
+import { addTicket } from '../actions'
 
 export default function CreateForm() {
-    const router = useRouter()
-
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [priority, setPriority] = useState('low')
-    const [isLoading, setIsLoading] = useState(false)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setIsLoading(true)
-
-        const ticket = { title, body, priority }
-
-        const res = await fetch('http://localhost:3000/api/tickets', {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(ticket)
-        })
-
-        const json = await res.json()
-
-        if (json.error) {
-            console.log(error.message)
-        }
-        if (json.data) {
-            router.refresh()
-            router.push('/tickets')
-        }
-    }
 
   return (
-    <form className="w-1/2" onSubmit={handleSubmit}>
+    <form action={addTicket} className="w-1/2">
         <label>
             <span>Title:</span>
             <input
+            name="title"
             required 
             type="text"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
             />
         </label>
         <label>
             <span>Body:</span>
             <textarea
+            name="body"
             required
-            onChange={(e) => setBody(e.target.value)}
-            value={body}
             />
         </label>
         <label>
             <span>Priority:</span>
             <select 
-            onChange={(e) => setPriority(e.target.value)}
-            value={priority}
+            name="priority"
             >
             <option value="low">Low Priority</option>
             <option value="medium">Medium Priority</option>
             <option value="high">High Priority</option>
             </select>
         </label>
-        <button 
-            className="btn-primary" 
-            disabled={isLoading}
-        >
-        {isLoading && <span>Adding...</span>}
-        {!isLoading && <span>Add Ticket</span>}
-        </button>
+        <SubmitButton />
     </form>
   )
 }
